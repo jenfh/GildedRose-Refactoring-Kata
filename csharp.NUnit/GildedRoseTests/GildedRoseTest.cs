fe912791c6ad76e0481a81865f95abd02e130fb1
Jennifer Bock <if23b125@technikum-wien.at>
2025-06-10 11:22:02 +0200

    Add test for standard item quality no negative quality

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -23,4 +23,13 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.That(items[0].Quality, Is.EqualTo(3));
     }
+
+    [Test]
+    public void StandardItem_No_Negative_Quality_Test()
+    {
+        var items = new List<Item> { new Item { Name = "Elixir of the Mongoose", SellIn = -2, Quality = 1 } };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
 }
