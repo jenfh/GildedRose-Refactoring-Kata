ef25c5b8a175de6f138b42523ab72679c4b83f72
MemoriJemand <if23b021@technikum-wien.at>
2025-06-11 16:03:41 +0200

    finished aged brie coverage

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -51,4 +51,31 @@ public class GildedRoseTest
         Assert.That(items[0].Quality, Is.EqualTo(1));
     }
 
+    [Test]
+    public void AgedBrie_Quality_Fast_Increasing_Test()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -1, Quality = 0 } };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void AgedBrie_Max_Quality_Test()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 } };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(50));
+    }
+
+    [Test]
+    public void AgedBrie_Decreasing_SellIn_Test()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 50 } };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].SellIn, Is.EqualTo(-1));
+    }
+
 }
